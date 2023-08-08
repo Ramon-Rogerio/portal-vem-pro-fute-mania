35d92d1969057e828924aa27dbb045f98d79077d
MarcoSilva0 <marcoantonio02016@outlook.com>
2023-08-07 22:09:07 -0300

    feat: Create contact page and route

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,10 +1,12 @@
 import { Routes as Routers, Route } from 'react-router-dom';
 import Index from '../pages/Index';
+import Contact from '../pages/Contact';
 
 export default function Routes() {
     return (
         <Routers>
             <Route path="/" element={<Index />} />
+            <Route path="/contato" element={<Contact />} />
         </Routers>
     )
 }
