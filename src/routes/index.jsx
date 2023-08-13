a7ac5fbe917d5380ed1e7a0e28762f07b9644b6f
MarcoSilva0 <marcoantonio02016@outlook.com>
2023-08-13 18:36:54 -0300

    feat: page register

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,12 +1,14 @@
 import { Routes as Routers, Route } from 'react-router-dom';
 import Index from '../pages/Index';
 import Contact from '../pages/Contact';
+import Register from '../pages/Register/Index';
 
 export default function Routes() {
     return (
         <Routers>
             <Route path="/" element={<Index />} />
             <Route path="/contato" element={<Contact />} />
+            <Route path="/registrar-workspace" element={<Register />} />
         </Routers>
     )
 }
