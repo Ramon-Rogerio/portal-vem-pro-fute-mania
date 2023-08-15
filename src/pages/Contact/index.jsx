7acdb69059168d83687aa15543f7620b9f764e28
lucastqa <lukinhafernandes32@gmail.com>
2023-08-15 20:30:49 -0300

    feat: Adjustment in Admin component

--- src/pages/Contact/index.jsx
+++ src/pages/Contact/index.jsx
@@ -1,4 +1,4 @@
-import Header from "../../components/organisms/Header";
+import Header from "../../components/organisms/Admin";
 
 export default function Contact() {
     return (
--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -2,6 +2,8 @@ import { Routes as Routers, Route } from 'react-router-dom';
 import Index from '../pages/Index';
 import Contact from '../pages/Contact';
 import Register from '../pages/Register/Index';
+import Header from '../components/organisms/Admin';
+import Admin from '../components/organisms/Admin';
 
 export default function Routes() {
     return (
@@ -9,6 +11,8 @@ export default function Routes() {
             <Route path="/" element={<Index />} />
             <Route path="/contato" element={<Contact />} />
             <Route path="/registrar-workspace" element={<Register />} />
+            <Route path="/gestor" element={<Admin><>Teste</></Admin>} >
+            </Route>
         </Routers>
     )
 }
